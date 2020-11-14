3de4a5c1d2b3eeeacebdd9ac0bd6b3b369443f87
dttinh18it3 <dttinh.18it3@sict.udn.vn>
2020-11-14 10:34:34 +0700

    update login account

--- src/emailClient/Controllers/HomeController.java
+++ src/emailClient/Controllers/HomeController.java
@@ -24,6 +24,8 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,7 +35,6 @@ public class HomeController {
     public static final String HOST_NAME = "smtp.gmail.com";
     public static final int SSL_PORT = 465;
     public static final int TSL_PORT = 587;
-    public static final String MY_EMAIL = "kurobakaito2708@gmail.com";
 
 
     private ObservableList<String> optionMail = FXCollections.observableArrayList("TO", "CC", "BCC");
@@ -43,6 +44,10 @@ public class HomeController {
     @FXML
     private TextField tfSubject;
     @FXML
+    private TextField tfLoginEmail;
+    @FXML
+    private TextField tfLoginPassword;
+    @FXML
     private ChoiceBox<String> optionMailChoiceBox = new ChoiceBox<>();
     @FXML
     private FontAwesomeIcon iconErrorRecipient = new FontAwesomeIcon();
@@ -51,8 +56,15 @@ public class HomeController {
     @FXML
     private HTMLEditor mailContent = new HTMLEditor();
 
+
+
     private Stage stageDialog;
+    private static Stage dialogStage;
     private Main mainApp;
+    private static Session session;
+    public static String login_email = "";
+    public static String login_password = "";
+    private static Properties properties;
 
     public HomeController() {
     }
@@ -62,44 +74,85 @@ public class HomeController {
         loadOptionMail();
     }
 
-    public void setupEmail() {
-        Properties properties = new Properties();
+    public boolean connectAuthenticatedToServer() {
+        properties = new Properties();
         properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", HOST_NAME);
         properties.put("mail.smtp.socketFactory.port", SSL_PORT);
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         properties.put("mail.smtp.port", SSL_PORT);
 
-        String myEmail = "kurobakaito2708@gmail.com";
-        String myPassword = "trannguyenkieulien";
+        login_email = tfLoginEmail.getText().trim();
+        login_password = tfLoginPassword.getText().trim();
 
-        // get Session
-        Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(myEmail, myPassword);
-            }
-        });
 
-        sendMessage(session);
-        System.out.println("Message sent successfully");
+        try {
+            // set Session
+            session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(login_email, login_password);
+                }
+            });
+
+            // login with google account
+            Transport transport = null;
+            System.out.println(session);
+            transport = session.getTransport("smtps");
+            transport.connect (HOST_NAME, SSL_PORT, login_email, login_password);
+            System.out.println("Login successfully !!");
+            // login successfully => show home layout
+            mainApp.showHomeLayout();
+            return true;
+        } catch (NoSuchProviderException e) {
+            System.out.println("Login failure 1 !! ");
+            e.printStackTrace();
+            return false;
+        } catch (MessagingException e) {
+            //login failure
+            System.out.println("Login failure 2 !!");
+            e.printStackTrace();
+            return false;
+        }
     }
 
-
+    //checkbox option mail (TO/CC/BCC)
     private void loadOptionMail() {
         optionMailChoiceBox.setItems(optionMail);
         optionMailChoiceBox.setValue("TO");
     }
 
+    //button send mail
     @FXML
     private void handleSendMail() {
         if (validateData()) {
             String recipientValue = tfRecipient.getText().trim();
             String subjectValue = tfSubject.getText().trim();
             System.out.println(recipientValue + " | " + subjectValue);
-            setupEmail();
+            try {
+                sendMessage(session);
+                dialogStage.close();
+            }catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            showAlert("Cảnh báo!", "Vui lòng nhập đầy đủ thông tin thư!");
+        }
+    }
+    //button login
+    @FXML
+    public void handleLoginValidate() {
+        if (connectAuthenticatedToServer()) {
+            mainApp.showHomeLayout();
+
+        }
+        else {
+            showAlert("Đăng nhập thất bại!", "Sai tài khoản hoặc mật khẩu!");
         }
     }
 
+
+    //validate fields data of send mail dialog
     private boolean validateData() {
         String recipientValue = tfRecipient.getText().trim();
         String subjectValue = tfSubject.getText().trim();
@@ -127,7 +180,7 @@ public class HomeController {
         alertWarning.initOwner(stageDialog);
         alertWarning.setTitle(title);
         alertWarning.setContentText(content);
-        alertWarning.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+        alertWarning.getDialogPane().getButtonTypes().add(ButtonType.OK);
         alertWarning.showAndWait();
     }
     
@@ -137,13 +190,12 @@ public class HomeController {
     }
 
     private void showCreateMailDialog() {
-
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(HomeController.class.getResource("../Views/fxml/sendMailDialog.fxml"));
             AnchorPane mailDialogPane = (AnchorPane) loader.load();
 
-            Stage dialogStage = new Stage();
+            dialogStage = new Stage();
             dialogStage.setTitle("Soạn thư");
             dialogStage.initModality(Modality.WINDOW_MODAL);
             dialogStage.initOwner(Main.primaryStage);
@@ -163,13 +215,13 @@ public class HomeController {
         try {
             String recipient = tfRecipient.getText();
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(MY_EMAIL));
+            message.setFrom(new InternetAddress(login_email));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
             message.setSubject(tfSubject.getText());
             message.setContent(mailContent.getHtmlText(), "text/html");
             Transport.send(message);
         } catch (Exception e) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, String.valueOf(e), e);
         }
         return null;
     }
--- src/emailClient/Main.java
+++ src/emailClient/Main.java
@@ -25,11 +25,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
-        primaryStage.setTitle("EC | Trang chủ");
+        primaryStage.setTitle("EC Client");
         primaryStage.getIcons().add(new Image("file:src/emailClient/Views/images/EC_icon.png"));
 
         initDashboardLayout();
-        showHomeLayout();
+        showLoginLayout();
     }
 
     public Main() {
@@ -66,29 +66,21 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+//    show login layout into the center of dashboard layout
+    public void showLoginLayout() {
+        try {
+            FXMLLoader loadder = new FXMLLoader();
+            loadder.setLocation(Main.class.getResource(("Views/fxml/loginForm.fxml")));
+            AnchorPane loginLayout = (AnchorPane) loadder.load();
+            //set login layout into the center of dashboard layout
+            dashboardLayout.setCenter(loginLayout);
+            HomeController controller = loadder.getController();
+            controller.setMainApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-//    public void showCreateMailDialog() {
-//        try {
-//            FXMLLoader loader = new FXMLLoader();
-//            loader.setLocation(Main.class.getResource("Views/fxml/sendMailDialog.fxml"));
-//            AnchorPane mailDialogPane = (AnchorPane) loader.load();
-//
-//            Stage dialogStage = new Stage();
-//            dialogStage.setTitle("Soạn thư");
-//            dialogStage.initModality(Modality.WINDOW_MODAL);
-//            dialogStage.initOwner(primaryStage);
-//            Scene scene = new Scene(mailDialogPane);
-//            dialogStage.setScene(scene);
-//
-//
-//            HomeController controller = loader.getController();
-//            controller.setDialog(dialogStage);
-//            dialogStage.showAndWait();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     public static void main(String[] args) {
         launch(args);
